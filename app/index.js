b2ed308735204fdb3931b6e1ff3b5a9e233ba40b
Kimanzi Mati <kimanzimati@kmatimac.local>
2016-05-12 15:41:27 -0400

    Added the MyTemplateViewComponent as well as refactored the styling and template loading (also affected MyViewComponent)

--- app/index.js
+++ app/index.js
@@ -2,7 +2,12 @@
  * The main entry point for the client-side app
  */
 import { Affront } from 'affront';
-import { DumbComponent, AtomComponent, MyViewComponent } from '../lib';
+import {
+	DumbComponent,
+	AtomComponent,
+	MyViewComponent,
+	MyTemplateViewComponent
+} from '../lib';
 
 Affront.Router.addComponent(new DumbComponent('/molecule'));
 
@@ -10,3 +15,4 @@ Affront.Router.addComponent(new AtomComponent('/atom'));
 Affront.Router.addComponent(new AtomComponent('/atom/:elementSymbol'));
 
 Affront.Router.addComponent(new MyViewComponent('/atom', document.getElementById('divMyView')));
+Affront.Router.addComponent(new MyTemplateViewComponent('/atom', document.getElementById('divMyTemplateView')));
