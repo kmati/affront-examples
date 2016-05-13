8080b710a71745a102d51683faad90bf1b7b077d
Kimanzi Mati <kimanzimati@kmatimac.local>
2016-05-12 20:04:22 -0400

    Modified the route of the MyTemplateViewComponent instance

--- app/index.js
+++ app/index.js
@@ -9,10 +9,14 @@ import {
 	MyTemplateViewComponent
 } from '../lib';
 
+// add the components of the app
 Affront.Router.addComponent(new DumbComponent('/molecule'));
 
 Affront.Router.addComponent(new AtomComponent('/atom'));
 Affront.Router.addComponent(new AtomComponent('/atom/:elementSymbol'));
 
 Affront.Router.addComponent(new MyViewComponent('/atom', document.getElementById('divMyView')));
-Affront.Router.addComponent(new MyTemplateViewComponent('/atom', document.getElementById('divMyTemplateView')));
+Affront.Router.addComponent(new MyTemplateViewComponent('/', document.getElementById('divMyTemplateView')));
+
+// now start the app!
+Affront.start();
